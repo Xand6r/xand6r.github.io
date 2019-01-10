e88bc79e1626d08b669044bb3c19dd01f1ac24fc
xand6r <professor.alex88@gmail.com>
2019-01-10 10:38:24 +0300

    refactored adding of projects using vue

--- js/script.js
+++ js/script.js
@@ -8,7 +8,14 @@ const app=new Vue({
     el:"#app",
     data:{
         current:"home",
-        cgpa:4.66
+        cgpa:4.66,
+        projectsInfo:[
+            {
+                picturePath:"./images/Project1.PNG",
+                summary:"A redesign of netflix's page of the american sniper's movie.",
+                link:"https://xand6r.neocities.org"
+            }
+        ]
     },
     methods: {
         setCurrent(str){
