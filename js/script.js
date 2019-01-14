79e844839401e4b1afb005e47683cbf224159726
xand6r <professor.alex88@gmail.com>
2019-01-14 21:33:00 +0300

    full-code

--- js/script.js
+++ js/script.js
@@ -14,6 +14,11 @@ const app=new Vue({
                 picturePath:"./images/Project1.PNG",
                 summary:"A redesign of netflix's page of the american sniper's movie.",
                 link:"https://xand6r.neocities.org"
+            },
+            {
+                picturePath:"./images/Project2.PNG",
+                summary:"A Thrilling game of tic-tac-toe against an AI. Implemented using Vue.js. If you win 10 times in a row do not hire me!",
+                link:"https://hexano.epizy.com"
             }
         ]
     },
